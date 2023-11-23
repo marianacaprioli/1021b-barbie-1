61273dbf7022bfdd9527ed4d9609571b36e416f2
marianacaprioli <mariana.caprioli@estudante.ifms.edu.br>
2023-11-23 14:17:48 -0400

    final

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,64 +1,61 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filme from "../filme/Filme";
-import './Main.css'
+import axios from "axios";
+import './Main.css';
+
 type FilmesType = {
-    id: number,
-    titulo:string,
-    sinopse:string,
-    imagem:string
-}
-export default function Main(){
-    //Hook
-    const [texto,setTexto] = useState("")
+  id: number;
+  titulo: string;
+  descricao: string;
+  imagem: string;
+};
 
-    const filmes:FilmesType[] = [
-        {
-            id:1,
-            titulo:"Barbie",
-            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
-            imagem:"/barbie.png"
-        },
-        {
-            id:2,
-            titulo:"Transformers Maiza",
-            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
-            imagem:"/transformes.jpeg"
-        },
-        {
-            id:3,
-            titulo:"Transformers Maiza",
-            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
-            imagem:"/transformes.jpeg"
+export default function Main() {
+  const [filmes, setData] = useState<FilmesType[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [texto, setTexto] = useState("");
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/filmes')
+      .then(response => {
+        if (response.status === 200) {
+          setData(response.data);
         }
+      })
+      .catch(error => {
+        console.error("Erro", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-    ]
-    //A função recebe um atributo chamado e de "event"
-    function mudaTexto(e:React.ChangeEvent<HTMLInputElement>){
-        console.log(e.target.value)
-        setTexto(e.target.value)
-    }
-    return(
-        <>
-            <div className="pesquisa">
-                
-                <p>Buscar Filme</p>
-                <input className='barrapesquisa' type="text" onChange={mudaTexto} />
-                <div>
-                    <p className='texto_digitado'>pesquisa: {texto}</p>
-                </div>
-            </div>
-            <main className="content-main">
-                {filmes
-                .filter((filme)=>filme.titulo.toLowerCase().includes(texto.toLowerCase()))
-                .map((filme:FilmesType)=>
-                    <Filme key={filme.id} 
-                           titulo={filme.titulo} 
-                           sinopse={filme.sinopse} 
-                           imagem={filme.imagem}
-                    />
-                    )
-                }
-            </main>
-        </>
-    )
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTexto(e.target.value);
+  };
+
+  if (loading) return "loading...";
+  if (error) return "error";
+
+  return (
+    <>
+      <div className="pesquisa">
+        <p>Buscar Filme</p>
+        <input className='barrapesquisa' type="text" onChange={handleTextChange} />
+        <div>
+          <p className='texto_digitado'>pesquisa: {texto}</p>
+        </div>
+      </div>
+      <main className="content-main">
+        {filmes &&
+          filmes
+            .filter((filme) => filme.titulo.toLowerCase().includes(texto.toLowerCase()))
+            .map((filme) => (
+              <Filme key={filme.id} titulo={filme.titulo} descricao={filme.descricao} imagem={filme.imagem} />
+            ))}
+      </main>
+    </>
+  );
 }
